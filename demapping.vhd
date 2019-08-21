c15ce35a5eb6f119b07268bc27601942f588cbe5
Ericson Joseph <ericsonjoseph@gmail.com>
2019-08-20 21:30:54 -0300

    Falta arreglar partes negativas de PSK

--- demapping.vhd
+++ demapping.vhd
@@ -41,13 +41,13 @@ signal z_i: std_logic_vector(N-1 downto 0) := (others =>'0');
 signal x_o: std_logic_vector(N-1 downto 0);
 signal y_o: std_logic_vector(N-1 downto 0);
 signal z_o: std_logic_vector(N-1 downto 0);
-signal zz_o: std_logic_vector(N-1 downto 0) := (others =>'0');	
+signal zz_o: integer;
 signal dv: 	std_logic;
 
 
 begin
 
-	zz_o <= z_o(N-1 downto 3) & "000";
+	zz_o <= to_integer(signed(z_o));
 
 	comp_cordic: cordic
 		generic map (
@@ -66,18 +66,22 @@ begin
 			z_o => z_o
 		);
 
-		mux: process(clk, rst, z_o) is
+		mux: process(dv) is
 		begin
-			if rising_edge(clk) then
-				case to_integer(signed(z_o)) is
-					when 0 =>
-						data <= "0000";
-					when others =>
-						data <= "0000";
-				end case;
+			if rising_edge(dv) then
+				if    ((zz_o > -18432) and (zz_o <= -14336)) then  data <= "1100";
+				elsif ((zz_o > -14336) and (zz_o <= -10240)) then  data <= "1101";
+				elsif ((zz_o > -10240) and (zz_o <= -6144))  then  data <= "1110";
+				elsif ((zz_o > -6144)  and (zz_o <= -2048))  then  data <= "1111";
+				elsif ((zz_o > -2048)  and (zz_o <= 2048))   then  data <= "0000";
+				elsif ((zz_o > 2048)   and (zz_o <= 6144))   then  data <= "0001";
+				elsif ((zz_o > 6144)   and (zz_o <= 10240))  then  data <= "0010";
+				elsif ((zz_o > 10240)  and (zz_o <= 14336))  then  data <= "0011";
+				elsif ((zz_o > 14336)  and (zz_o <= 18432))  then  data <= "0100";
+				else 
+					data <= "1111";
+				end if;
 			end if;
 		end process;
 			
 end demapping_arc;
-
-
